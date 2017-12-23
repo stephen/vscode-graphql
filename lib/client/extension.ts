2fd63096277818fec61ffc8e885bd919cffcd322
Stephen Wan <stephen@stephenwan.com>
2017-12-23 13:44:40 -0600

    lib: add support for graphql-language-service-server

---
+++ lib/client/extension.ts
@@ -0,0 +1,42 @@
+import * as path from "path";
+
+import { workspace, ExtensionContext } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient";
+
+export function activate(context: ExtensionContext) {
+  let serverModule = context.asAbsolutePath(
+    path.join("bin/server", "server.js"),
+  );
+
+  let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
+
+  let serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "graphql" }],
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher("**/*.graphql"),
+    },
+  };
+
+  let disposable = new LanguageClient(
+    "lspGraphql",
+    "GraphQL Language Server",
+    serverOptions,
+    clientOptions,
+  ).start();
+
+  context.subscriptions.push(disposable);
+}
